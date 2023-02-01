f6563302e78979a9858f0f1e6d1f0a6512e58f0a
Oskar Legner <oskarlegner@gmail.com>
2023-02-01 01:14:00 +0100

    Init with action plan

---
+++ main.py
@@ -0,0 +1,33 @@
+if __name__ == "__main__":
+    print("Let's go!")
+
+
+    # ---------------- #
+    #   Action plan!   #
+    # ---------------- #
+
+    # Define mode:
+    # For MVP only Mon-Fri (work-days mode)
+
+    # Define range (how many days backwards)
+
+    # Try to create separate repo
+
+    # Try to create commit
+
+    # Try to create commit for the past
+
+    # Calculate week days
+
+    # Create commit for every week day
+
+
+
+    # ---------------- #
+    #     Later...     #
+    # ---------------- #
+
+    # Use some github API to get only non-filled days
+
+    # Maybe add mode for filling just "alone" days
+
