8851fef5b5104ed653f2ae9a8e57d38f992bdd88
Oskar Legner <oskarlegner@gmail.com>
2023-02-01 01:56:54 +0100

    Add consts sketch

--- args.py
+++ args.py
@@ -10,8 +10,12 @@ import argparse
 def use_args():
     parser = argparse.ArgumentParser(prog="Wingman", description='Take care of your git activity log')
 
-    parser.add_argument('backrange', metavar='RANGE', type=int, help='influence backrange in days')
-    parser.add_argument('-n', metavar="REPO_NAME", dest='repo_name', help='set the cellar repo name')
-    parser.add_argument('-m', dest='mode', help='set the cellar repo name')
+    parser.add_argument('backrange', metavar='RANGE', type=int, help='Influence backrange in days')
+    parser.add_argument('-n', metavar="REPO_NAME", dest='repo_name', help='Set the cellar repo name')
+    parser.add_argument('-m', dest='mode', help='Set the mode. Use consts:')
+    parser.add_argument('WEEK', action='store_const', const="week", help='Only Mon-Fri')
+    parser.add_argument('WEEKEND', action='store_const', const="weekend", help='Only Sat-Sun')
+    parser.add_argument('ALL', action='store_const', const="all", help='All days of the week')
+
 
     return parser.parse_args()
